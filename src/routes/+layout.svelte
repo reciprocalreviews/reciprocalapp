deb1667c8153e43826d04bb8c84c35e22a875978
Amy J. Ko <ajko@uw.edu>
2023-10-15 13:03:01 -0700

    Basic site placeholder.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<slot />
+
+<style>
+	:global(body) {
+		font-family: 'Josefin Sans', sans-serif;
+		padding: 3em;
+		max-width: 40em;
+	}
+
+	:global(h1) {
+		font-size: 24pt;
+		font-weight: 500;
+		margin-top: 2em;
+	}
+
+	:global(p) {
+		font-size: 16pt;
+		font-weight: 300;
+		line-height: 1.4;
+	}
+
+	:global(a) {
+		color: black;
+		text-decoration: underline;
+		text-decoration-thickness: 2px;
+	}
+
+	:global(a:hover) {
+		color: black;
+		text-decoration: underline;
+		text-decoration-thickness: 4px;
+	}
+
+	:global(a:focus) {
+		color: black;
+		text-decoration: underline;
+		text-decoration-thickness: 4px;
+		outline: none;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Reciprocal Reviews</h1>
+<p>
+	We're a community of academics striving for more equitable and sustainable academic peer review.
+	This page will be a place to find reviewers and reviewing opportunities, across academia. Visit <a
+		href="https://github.com/reciprocalreviews">GitHub</a
+	> to learn more and contribute.
+</p>
