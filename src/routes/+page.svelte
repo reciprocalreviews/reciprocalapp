c71e7eb8c4bcc7dba101ed287be55fba925896dc
Amy J. Ko <ajko@uw.edu>
2023-10-18 12:33:23 -0700

    Added newsletter.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <h1>Reciprocal Reviews</h1>
 <p>
-	We're a community of academics striving for more equitable and sustainable academic peer review.
-	This page will be a place to find reviewers and reviewing opportunities, across academia. Visit <a
-		href="https://github.com/reciprocalreviews">GitHub</a
-	> to learn more and contribute.
+	We're a volunteer community of academics striving for more equitable and sustainable academic peer
+	review. This site will eventually be a platform for finding reviewers and reviewing opportunities,
+	across different communities in academia. Visit <a href="https://github.com/reciprocalreviews"
+		>GitHub</a
+	>
+	to learn more and contribute or subscribe to our newsletter:
 </p>
+
+<iframe
+	title="Newsletter subscription"
+	src="https://reciprocalreviews.substack.com/embed"
+	width="480"
+	height="320"
+	style="border:1px solid #EEE; background:white;"
+	frameborder="0"
+	scrolling="no"
+/>
