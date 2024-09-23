4729567b553998507cee96984fbc6dd712c0c549
Andrew Petersen <andrew.petersen@utoronto.ca>
2024-09-23 12:00:14 -0400

    Adding copyright notices to source code

--- tests/test.ts
+++ tests/test.ts
@@ -1,3 +1,9 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
