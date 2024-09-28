e7c310a5d78d462a9a9fe1c231c0e19f70b380cf
Amy J. Ko <ajko@uw.edu>
2024-09-28 15:59:50 -0700

    Page wrapper for home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,60 +3,63 @@
 	import Lead from '$lib/components/Lead.svelte';
 	import Link from '$lib/components/Link.svelte';
 	import Tag from '$lib/components/Tag.svelte';
+	import Page from '$lib/components/Page.svelte';
 </script>
 
 <svelte:head>
 	<title>Reciprocal Reviews</title>
 </svelte:head>
 
-<h1>Reciprocal Reviews</h1>
-{#if dev}
-	<Lead>Incentivize on-time, quality peer reviews.</Lead>
-	<p>
-		Academia incentivizes publishing over reviewing. That means too many submissions and not enough
-		reviews, or reviews that are late and low quality.
-	</p>
+<Page title="Reciprocal Reviews">
+	<h1>Reciprocal Reviews</h1>
+	{#if dev}
+		<Lead>Incentivize on-time, quality peer reviews.</Lead>
+		<p>
+			Academia incentivizes publishing over peer review. That means too many submissions and not
+			enough reviews, or reviews that are late and low quality.
+		</p>
 
-	<p>
-		<strong>Reciprocal Reviews</strong> breaks this cycle:
-	</p>
+		<p>
+			<strong>Reciprocal Reviews</strong> breaks this cycle:
+		</p>
 
-	<ol>
-		<li>
-			<strong>Editors set an cost for submission</strong>, priced in <Tag>review tokens</Tag>.
-		</li>
-		<li>
-			<strong>Authors earn tokens</strong> by volunteering for venues, sharing expertise and availabilty,
-			and submitting on-time, quality reviews.
-		</li>
-		<li>
-			<strong>Authors spend tokens</strong> to submit manuscripts for review, splitting the cost with
-			co-authors. Or, they can gift tokens to colleagues in or donate their tokens to the community pool
-			for strategic investiments in equity and emerging areas.
-		</li>
-	</ol>
+		<ol>
+			<li>
+				<strong>Editors set an cost for submission</strong>, priced in <Tag>review tokens</Tag>.
+			</li>
+			<li>
+				<strong>Authors earn tokens</strong> by volunteering for venues, sharing expertise and availability,
+				and submitting on-time, quality reviews.
+			</li>
+			<li>
+				<strong>Authors spend tokens</strong> to submit manuscripts for review, splitting the cost with
+				co-authors. Or, they can gift tokens to colleagues in or donate their tokens to the community
+				pool for strategic investiments in equity and emerging areas.
+			</li>
+		</ol>
 
-	<p>
-		See the <Link to="/sources">journals and conferences</Link> using the platform and reach out to their
-		editors for their experiences. Or, <Link to="login">log in</Link> to propose a journal or conference
-		for adoption.
-	</p>
+		<p>
+			See the <Link to="/sources">journals and conferences</Link> using the platform and reach out to
+			their editors for their experiences. Or, <Link to="login">log in</Link> to propose a journal or
+			conference for adoption.
+		</p>
 
-	<p>
-		<em>Don't want to set a cost yet?</em> Use Reciprocal Reviews to track volunteer reviewers in your
-		community.
-	</p>
-{:else}
-	<p>
-		We're a volunteer community of academics striving for more equitable and sustainable academic
-		peer review. This site will eventually be a platform for tracking reviewer availabilty and
-		contributions in a way that ensures sufficient reviewing labor for submissions. Visit <Link
-			to="https://github.com/reciprocalreviews">GitHub</Link
-		>
-		to learn more and contribute or subscribe to our twice monthly
-		<Link to="https://reciprocalreviews.substack.com/">newsletter</Link>:
-	</p>
-{/if}
+		<p>
+			<em>Don't want to set a cost yet?</em> Use Reciprocal Reviews to track volunteer reviewers in your
+			community.
+		</p>
+	{:else}
+		<p>
+			We're a volunteer community of academics striving for more equitable and sustainable academic
+			peer review. This site will eventually be a platform for tracking reviewer availabilty and
+			contributions in a way that ensures sufficient reviewing labor for submissions. Visit <Link
+				to="https://github.com/reciprocalreviews">GitHub</Link
+			>
+			to learn more and contribute or subscribe to our twice monthly
+			<Link to="https://reciprocalreviews.substack.com/">newsletter</Link>:
+		</p>
+	{/if}
+</Page>
 
 <h2>Updates</h2>
 
