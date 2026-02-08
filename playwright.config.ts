0e59dcdb2c659e5f849dd4f2d7cd165cebdff482
Amy J. Ko <ajko@uw.edu>
2026-02-08 13:32:06 -0800

    Drop Firefox and WebKit from Playwright tests for now.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { type PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
+		// Sync types, build with vite, run the preview server, and start Supabase locally, without services we don't use.
 		command:
 			'npm run sync && npm run build && npm run preview && supabase db start --exclude storage-api,imgproxy,logflare,supavisor',
 		name: 'dev',
@@ -16,21 +17,21 @@ const config: PlaywrightTestConfig = {
 			use: {
 				...devices['Desktop Chrome']
 			}
-		},
-
+		}
+		/** Roll the dice and only test on Chromium until we have a more stable test suite.
 		{
 			name: 'firefox',
 			use: {
 				...devices['Desktop Firefox']
 			}
 		},
-
 		{
 			name: 'webkit',
 			use: {
 				...devices['Desktop Safari']
 			}
 		}
+		*/
 	]
 };
 
