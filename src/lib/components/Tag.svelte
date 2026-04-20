f284609cef36b27e361d37ab067105851596bc3b
Amy J. Ko <ajko@uw.edu>
2026-04-19 17:11:40 -0700

    Redesigned tag color.

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -4,7 +4,7 @@
 	.tag {
 		font-family: var(--font-face);
 		background: var(--salient-color-faded);
-		color: var(--background-color);
+		color: var(--foreground-color);
 		padding: calc(var(--spacing) / 4) var(--spacing-half);
 		border-radius: 0.5em;
 		font-size: var(--extra-small-font-size);
